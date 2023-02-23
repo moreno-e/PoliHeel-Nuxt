40464f8b5a960462c05337a2d9fae5785312888f
Eric Moreno <ericmoreno@Erics-MacBook-Pro.local>
2023-02-22 20:29:09 -0500

    added navbar and basic styling

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-carolina-blue">
+            <nav class="container mx-auto p-4 flex justify-between text-carolina-navy">
+                <NuxtLink to="/" class="font-bold">PoliHeel</NuxtLink>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- here is where we want to output page content -->
+        <div class="container mx-auto p-4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+.router-link-exact-active {
+    color: white
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'carolina-blue': '#4B9CD3',
+          'carolina-navy': '#13294B',
+        },
+      }
+    }
+  }
+  
+  
