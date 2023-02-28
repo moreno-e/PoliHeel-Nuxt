fe396f613fba83049c5931e0682c00df2aca5009
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-02-27 21:27:00 -0500

    grabbed img sources for each member

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,57 @@
     <h1 class="text-center">
       Welcome to PoliHeel
     </h1>
-    <p>{{ data }}</p>
+    <img :src="imgs[0].source" alt="hi">
   </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
     const { data } = await useFetch('/api/googlecivic');
+    const civicData = ref(data);
+
+    const getPartialOffices = () => {
+      let { officialIndices: [lastOfficeIdx] } =
+        civicData.value.offices.find(office => office.name.includes('Governor of'));
+
+      const adjustedIdx = lastOfficeIdx += 1;
+      const partialOffices = civicData.value.officials.slice(0, adjustedIdx);
+
+      return partialOffices;
+    };
+
+    const partialOffices = getPartialOffices();
+
+    const getWikiNameParams = () => {
+      const srcs = partialOffices.map((office) => {
+        const wikiURL = office.urls[1];
+        const wikiName = wikiURL.slice(30);
+
+        return wikiName;
+      });
+
+      return srcs;
+    };
+
+    const nameParams = getWikiNameParams();
+    const promises = [];
+
+    for (const name of nameParams) {
+      const uri = `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=${name}`;
+
+      promises.push(useFetch(uri));
+    }
+
+    const responses = await Promise.all(promises);
+    const imgs = [];
+
+    for (const response of responses) {
+      const { data } = response;
+      const picture = ref(data);
+
+      imgs.push(picture.value.query.pages[0].original);
+    }
+
 </script>
 
 <style scoped>
--- server/api/googlecivic.js
+++ server/api/googlecivic.js
@@ -2,14 +2,7 @@
 export default defineEventHandler(async () => {
     const { googleCivicKey } = useRuntimeConfig();
     const uri = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=27514&key=${googleCivicKey}`;
-    const { offices, officials } = await $fetch(uri);
+    const response = await $fetch(uri);
 
-    let { officialIndices: [lastOfficeIdx] } =
-        offices.find(office => office.name.includes('Governor of'));
-
-    const adjustedIdx = lastOfficeIdx += 1;
-    const partialOffices = officials.slice(0, adjustedIdx);
-
-    // returning only a portion of the offices for the home page
-    return partialOffices;
+    return response;
 });
