5c68c81bb73d42fc987bda5ab6a7a9ee5f2e8493
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-03-29 17:06:51 -0400

    updated how vars are declared

--- pages/index.vue
+++ pages/index.vue
@@ -5,33 +5,42 @@
       Below contains your current executive and legislative members. Along with
       your state governor
     </p>
-    <members-display :partial-offices="partialOffices" />
+    <members-display :partial-offices="state.partialOffices" />
   </div>
 </template>
 
 <script setup>
-  const { data: civicData } = await useFetch('/api/googlecivic');
-  const civicDataRef = ref(civicData);
+  const state = reactive({
+    civicData: '',
+    partialOffices: [],
+    nameParams: '',
+    promises: [],
+    responses: '',
+  });
+
+  const { data } = await useFetch('/api/googlecivic');
+
+  state.civicData = data;
 
   // get a users members
   const getPartialOffices = () => {
     let {
       officialIndices: [lastOfficeIdx],
-    } = civicDataRef.value.offices.find((office) =>
+    } = state.civicData.offices.find((office) =>
       office.name.includes('Governor of')
     );
 
     const adjustedIdx = (lastOfficeIdx += 1);
-    const partialOffices = civicDataRef.value.officials.slice(0, adjustedIdx);
+    const partialOffices = state.civicData.officials.slice(0, adjustedIdx);
 
     return partialOffices;
   };
 
-  const partialOffices = getPartialOffices();
+  state.partialOffices = getPartialOffices();
 
   // slice the members names to pass to wiki api for their image
   const getWikiNameParams = () => {
-    const params = partialOffices.map((office) => {
+    const params = state.partialOffices.map((office) => {
       const wikiURL = office.urls[1];
       const wikiName = wikiURL.slice(30);
 
@@ -42,22 +51,23 @@
   };
 
   // fetch current member images
-  const nameParams = getWikiNameParams();
-  const promises = [];
+  state.nameParams = getWikiNameParams();
+  state.promises = [];
 
-  for (const name of nameParams) {
+  for (const name of state.nameParams) {
     const uri = `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=${name}`;
 
-    promises.push(useFetch(uri));
+    state.promises.push(useFetch(uri));
   }
 
-  const responses = await Promise.all(promises);
+  state.responses = await Promise.all(state.promises);
 
-  for (const [index, response] of responses.entries()) {
+  for (const [index, response] of state.responses.entries()) {
     const { data } = response;
     const picture = ref(data);
 
-    partialOffices[index].portrait = picture.value.query.pages[0].original;
+    state.partialOffices[index].portrait =
+      picture.value.query.pages[0].original;
   }
 </script>
 
