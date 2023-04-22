cfe52daf026cb29e513c5662fba50726c893a2db
Eric Moreno <ericmoreno@Erics-MacBook-Pro.local>
2023-04-22 17:19:10 -0400

    updated random number count to include index 0

--- pages/index.vue
+++ pages/index.vue
@@ -30,12 +30,14 @@
   // If there is an error with IPStack call use default Zip Code
   const defaultZipCodes = [
     { stateOfZipCode: 'Texas', zipCode: 77449 },
+    { stateOfZipCode: 'Texas', zipCode: 79936 },
     { stateOfZipCode: 'New York', zipCode: 11368 },
-    { stateOfZipCode: 'Illinois', zipCode: 60629 }
+    { stateOfZipCode: 'Illinois', zipCode: 60629 },
+    { stateOfZipCode: 'New York', zipCode: 11385 }
   ];
 
   const defaultRandomZipCode = () => {
-    const rndInt = Math.floor(Math.random() * 3) + 1;
+    const rndInt = Math.floor(Math.random() * (5));
 
     return defaultZipCodes[rndInt];
   };
