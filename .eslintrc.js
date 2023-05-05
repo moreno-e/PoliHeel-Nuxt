0c8c2f49e22ff3b89166ab6b8ee0bb657cf79034
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-05-04 22:08:13 -0400

    updated linting rules -> indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,82 +1,84 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: ['@nuxtjs/eslint-config-typescript'],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true
-      }
-    }
-  ],
-  rules: {
-    indent: [0, 'spaces'],
-    'block-spacing': 'error',
-    'comma-spacing': ['error', { before: false, after: true }],
-    'eol-last': ['error', 'always'],
-    'key-spacing': 'error',
-    'keyword-spacing': ['error', { before: true, after: true }],
-    'max-len': ['error', { code: 100, ignoreUrls: true }],
-    'no-console': 'warn',
-    'no-template-curly-in-string': 'error',
-    'no-trailing-spaces': 'error',
-    'no-var': 'error',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'object-curly-spacing': ['error', 'always'],
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: '*', next: 'block-like' },
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      {
-        blankLine: 'any',
-        prev: ['const', 'let', 'var'],
-        next: ['const', 'let', 'var']
-      }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:vue/vue3-recommended'
     ],
-    'prefer-const': 'error',
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-    radix: 'error',
-    semi: ['error', 'always'],
-    'space-before-blocks': 'error',
-    'space-before-function-paren': [
-      'error',
-      { anonymous: 'never', named: 'never', asyncArrow: 'always' }
-    ],
-    'space-unary-ops': ['error', { words: true, nonwords: false }],
-    'spaced-comment': ['error', 'always'],
-    'vue/html-closing-bracket-newline': [
-      'error',
-      {
-        singleline: 'never',
-        multiline: 'never'
-      }
-    ],
-    'vue/first-attribute-linebreak': [
-      'error',
-      {
-        singleline: 'ignore',
-        multiline: 'below'
-      }
-    ],
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: {
-          max: 1
-        },
-        multiline: {
-          max: 1
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
         }
-      }
-    ]
-  }
+    ],
+    rules: {
+        indent: 'off',
+        'block-spacing': 'error',
+        'comma-spacing': ['error', { before: false, after: true }],
+        'eol-last': ['error', 'always'],
+        'key-spacing': 'error',
+        'keyword-spacing': ['error', { before: true, after: true }],
+        'max-len': ['error', { code: 100, ignoreUrls: true }],
+        'no-console': 'warn',
+        'no-template-curly-in-string': 'error',
+        'no-trailing-spaces': 'error',
+        'no-var': 'error',
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'object-curly-spacing': ['error', 'always'],
+        'padding-line-between-statements': [
+            'error',
+            { blankLine: 'always', prev: '*', next: 'return' },
+            { blankLine: 'always', prev: '*', next: 'block-like' },
+            { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+            {
+                blankLine: 'any',
+                prev: ['const', 'let', 'var'],
+                next: ['const', 'let', 'var']
+            }
+        ],
+        'prefer-const': 'error',
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+        radix: 'error',
+        semi: ['error', 'always'],
+        'space-before-blocks': 'error',
+        'space-before-function-paren': [
+            'error',
+            { anonymous: 'never', named: 'never', asyncArrow: 'always' }
+        ],
+        'space-unary-ops': ['error', { words: true, nonwords: false }],
+        'spaced-comment': ['error', 'always'],
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never',
+                multiline: 'never'
+            }
+        ],
+        'vue/first-attribute-linebreak': [
+            'error',
+            {
+                singleline: 'ignore',
+                multiline: 'below'
+            }
+        ],
+        'vue/max-attributes-per-line': [
+            'error',
+            {
+                singleline: {
+                    max: 1
+                },
+                multiline: {
+                    max: 1
+                }
+            }
+        ],
+        'vue/script-indent': ['error', 2, { baseIndent: 1 }]
+    }
 };
